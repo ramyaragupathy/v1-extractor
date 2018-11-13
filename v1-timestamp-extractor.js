c174d6fc9bc8bf44b0f7446da6fa4e29c7b13777
Ramya <r.ragupathy85@gmail.com>
2018-11-13 18:44:03 +0530

    Init

---
+++ v1-timestamp-extractor.js
@@ -0,0 +1,25 @@
+const csvFilePath = './refugee-list.csv'
+const csv = require('csvtojson')
+var xmlParser = require('xml2json')
+const request = require('request')
+var options = {
+  headers: {
+    'Accept': 'text/xml',
+    'Accept-Language': 'en'
+  }
+}
+
+csv({delimiter: ['\t']})
+  .fromFile(csvFilePath)
+  .then((jsonObj) => {
+    console.log('id, created')
+    jsonObj.forEach(ele => {
+      options.url = 'https://www.openstreetmap.org/api/0.6/node/' + ele['@id'] + '/history'
+      request(options, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+          var historyJson = JSON.parse(xmlParser.toJson(body))
+          console.log(historyJson.osm.node[0].id + ',' + historyJson.osm.node[0].timestamp)
+        }
+      })
+    })
+  })
